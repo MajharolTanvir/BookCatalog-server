85615fb3fc91194fec4d7ce3eec90bb5aea47d80
MajharolTanvir <majharul5456@gmail.com>
2023-07-16 01:00:37 +0600

    User model create successfully

---
+++ src/app/modules/User/user.model.ts
@@ -0,0 +1,29 @@
+/* eslint-disable @typescript-eslint/no-this-alias */
+import { Schema, model } from "mongoose";
+import { IUser, UserModel } from "./user.interface";
+
+const UserSchema = new Schema<IUser, UserModel>(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+    toJSON: {
+      virtuals: true,
+    },
+  },
+);
+
+export const User = model<IUser, UserModel>("User", UserSchema);
