a8b27d01dc7d71b2ae12d2e886d0cf6ddb76b3f8
MajharolTanvir <majharul5456@gmail.com>
2023-07-16 00:58:42 +0600

    User interface create successfully

--- src/app/modules/Book/book.route.ts
+++ src/app/modules/Book/book.route.ts
@@ -1,26 +1,12 @@
 import express from "express";
+// import { BookController } from "./book.controller";
 
 const router = express.Router();
 
 // router.post(
-//   "/create-student",
-//   validationRequest(UserValidation.createStudentZodSchema),
-//   auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
-//   UserController.createStudent,
-// );
-
-// router.post(
-//   "/create-faculty",
-//   validationRequest(UserValidation.createFacultyZodSchema),
-//   auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
-//   UserController.createFaculty,
-// );
-
-// router.post(
-//   "/create-admin",
-//   auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN),
-//   validationRequest(UserValidation.createAdminZodSchema),
-//   UserController.createAdmin,
+//   "/create-book",
+// //   auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+//   BookController.addNewBook,
 // );
 
 export const BookRoutes = router;
