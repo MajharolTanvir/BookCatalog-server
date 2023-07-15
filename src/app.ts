711efab3e6329c4bada0b3ebf7051e2c9d5a1b43
MajharolTanvir <majharul5456@gmail.com>
2023-07-15 23:05:02 +0600

    Eslint pretier Lint stage husky setup done

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,16 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
 app.use(cors());
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
 });
 
-export default app
+export default app;
